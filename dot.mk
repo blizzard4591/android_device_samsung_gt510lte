19747ba2e82093f6b83fa22d54077a63092b61a7
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2018-10-07 01:12:33 -0400

    gt510lte: Add dotOS Makefile
    Change-Id: I1f34ed172f05d1644f136af6d482a06dc5cf69a6

---
+++ dot.mk
@@ -0,0 +1,18 @@
+# Inherit from common
+$(call inherit-product, device/samsung/gte-common/dot.mk)
+
+$(call inherit-product, device/samsung/gt510lte/device.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1024
+TARGET_SCREEN_WIDTH := 768
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := gt510lte
+PRODUCT_NAME := dot_gt510lte
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-T555
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_CHARACTERISTICS := tablet
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
