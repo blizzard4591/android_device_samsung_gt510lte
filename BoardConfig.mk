ed3b41b13a709663a70d81d4c39e1d59489227b4
PBerger <berger@cs.rwth-aachen.de>
2021-04-03 16:34:53 +0200

    Fix mistake in OTA assert.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,7 +4,7 @@ include device/samsung/gte-common/BoardConfigCommon.mk
 LOCAL_PATH := device/samsung/gt510lte
 
 # Asserts
-TARGET_OTA_ASSERT_DEVICE := gt510lte,gt510ltexx,SM-T550
+TARGET_OTA_ASSERT_DEVICE := gt510lte,gt510ltexx,SM-T555
 
 # Kernel
 TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_gt510lte_eur_defconfig
